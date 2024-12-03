211b00a6832157295b2f8688050f20dfcc6f8aa1
Timeyan <kopatsa01@gmail.com>
2024-12-03 14:36:48 +0300

    Add LeaveRoom button

--- music_together/frontend/src/components/Room.js
+++ music_together/frontend/src/components/Room.js
@@ -1,32 +1,67 @@
-//import { response } from "express";
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Button, Grid2, Typography } from "@mui/material";
+import { useParams, useNavigate } from "react-router-dom";
 
-const Room = () => {
+const Room = ({ leaveRoomCallback }) => {
     const { roomCode } = useParams();
-    const [votesToSkip] = React.useState(2);
-    const [guestCanPause] = React.useState(false);
-    const [isHost] = React.useState(false);
+    const [votesToSkip, setVotesToSkip] = React.useState(2);
+    const [guestCanPause, setGuestCanPause] = React.useState(false);
+    const [isHost, setIsHost] = React.useState(false);
+    const navigate = useNavigate();
 
-    const getRoomDetails = () => {
-        fetch("/api/get-room" + "?code=" + roomCode).then((response) => 
-            response.json()
-        ).then((data) => {
-            this.setState({
-                votesToSkip: data.votes_to_skip,
-                guestCanPause: data.guest_can_pause,
-                isHost: data.is_host,
-            })
-        });
+    useEffect(() => {
+        getRoomDetails();
+    }, []);
+    const getRoomDetails = async () => {
+        const response = await fetch(`/api/get-room?code=${roomCode}`);
+        if (!response.ok) {
+            leaveRoomCallback();
+            navigate("/");
+            return;
+        }
+        const data = await response.json();
+        setGuestCanPause(data.guest_can_pause);
+        setVotesToSkip(data.votes_to_skip);
+        setIsHost(data.is_host);
+        console.log("yeah");
+    }
+
+    const leaveButtonPressed = () => {
+        console.log("click");
     }
 
     return (
-        <div>
-            <h3>{roomCode}</h3>
-            <p>Votes: {votesToSkip}</p>
-            <p>Guest can{guestCanPause ? "" : "'t"} Pause</p>
-            <p>Host is {isHost ? "" : "not "}me</p>
-        </div>
+        <Grid2 container spacing={1}>
+        <Grid2 size={{xs:12}} align="center">
+          <Typography variant="h4" compact="h4" component={'span'}>
+            Code: {roomCode}
+          </Typography>
+        </Grid2>
+        <Grid2 size={{xs:12}} align="center">
+          <Typography variant="h6" compact="h6" component={'span'}>
+            Votes: {votesToSkip}
+          </Typography>
+        </Grid2>
+        <Grid2 size={{xs:12}} align="center">
+          <Typography variant="h6" compact="h6" component={'span'}>
+            Guest Can Pause: {guestCanPause.toString()}
+          </Typography>
+        </Grid2>
+        <Grid2 size={{xs:12}} align="center">
+          <Typography variant="h6" compact="h6" component={'span'}>
+            Host: {isHost.toString()}
+          </Typography>
+        </Grid2>
+        <Grid2 size={{xs:12}} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={leaveButtonPressed}
+          >
+            Leave Room
+          </Button>
+        </Grid2>
+      </Grid2>
     );
 };
 
